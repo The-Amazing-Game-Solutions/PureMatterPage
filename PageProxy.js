bedeef7e0309dcf183b9def2cd725ca56aa36d2a
Negawisp <BlindEyed@yandex.ru>
2025-03-27 15:07:32 +0300

    0.7.9

---
+++ PageProxy.js
@@ -0,0 +1,457 @@
+var pageProxy = {};
+
+/** ~~~ getPlayerData ~~~ */
+
+pageProxy.playerData = {
+  "LastVisitTs": "2025-03-26 15:46:42",
+  "DaysInARow": 6,
+  "Grid": [
+		[
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			}
+		],
+		[
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_glass_5",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_fnl_3",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_paper_4",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_glass_4",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_food_2",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			}
+		],
+		[
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_cleaning_tools_5",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_3",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "s_supermarket_2",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_5",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "s_garage_1",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			}
+		],
+		[
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_4",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "s_supermarket_1",
+				"CellBlockedType": "SemiOpen",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "Open",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "s_supermarket_3",
+				"CellBlockedType": "SemiOpen",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_2",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			}
+		],
+		[
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "s_working_inventory_1",
+				"CellBlockedType": "SemiOpen",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "Open",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "Open",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "Open",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "s_working_inventory_2",
+				"CellBlockedType": "SemiOpen",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			}
+		],
+		[
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_cleaning_tools_4",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "SemiOpen",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "SemiOpen",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_paper_1",
+				"CellBlockedType": "SemiOpen",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_cleaning_tools_3",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			}
+		],
+		[
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_cleaning_tools_6",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_6",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_cleaning_tools_3",
+				"CellBlockedType": "SemiOpen",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_paper_2",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_3",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			}
+		],
+		[
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "s_garage_1",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_food_1",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_glass_3",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_protection_tools_3",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_glass_2",
+				"CellBlockedType": "Close",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			}
+		],
+		[
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			},
+			{
+				"ElementId": "r_plastic_1",
+				"CellBlockedType": "CloseBeforeUpdate",
+				"SpawnerCount": -1,
+				"TimerProgressSeconds": -1.0
+			}
+		]
+	],
+	"Energy": {
+		"Count": 40,
+		"TimerProgressSeconds": 60
+	},
+	"PlotProgress": {
+		"ChapterN": 0,
+		"EpisodeN": 0,
+		"TasksProgress": [
+		true,
+		true,
+		true
+		]
+	},
+	"MetaBoxIds": [
+		"r_plastic_2",
+		"s_garage_1"
+	],
+	"ChallengeEnergyRewardTaken": [
+		false, false, false
+	],
+	"InGameDDQuestsStatus": {
+		"FinishCh1Ep1_isComplete": false,
+		"Play7DaysInARow_isComplete": false,
+		"FinishCh2Ep1_isComplete": false,
+		"FinishCh5Ep14_isComplete": false,
+		"MakeRPlastic6_isComplete": false,
+		"MakeRGlass6_isComplete": false
+	}
+};
+
+/** Utility */
+pageProxy.eName_getPlayerDataInitial = "gamePageRequest_GetPlayerDataOnLoad";
+pageProxy.callbacks_getPlayerDataInitial = [];
+pageProxy.isWaiting_getPlayerDataInitial = false;
+
+pageProxy.getPlayerDataInitial = function(onResolved) {
+    pageProxy.callbacks_getPlayerDataInitial.push(onResolved);
+    if (pageProxy.isWaiting_getPlayerDataInitial) {
+        return;
+    }
+    const e = new Event(pageProxy.getPlayerDataOnLoadEName);
+    document.dispatchEvent(e);
+};
+
+pageProxy.onResolved_GetPlayerDataInitial = function(playerData) {
+    pageProxy.playerData = playerData;
+    pageProxy.callbacks_getPlayerDataInitial.forEach(callback => {
+        callback(playerData);
+    });
+};
+
+/** ~~~ getPlayerData ~~~ */
